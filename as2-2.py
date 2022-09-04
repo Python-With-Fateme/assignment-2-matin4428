3e718741e3f86cfaefc806abe9cb504080631559
matin hosseini <matinhosseini@matins-MacBook-Pro.local>
2022-09-04 08:32:54 +0430

    test

---
+++ as2-2.py
@@ -0,0 +1,21 @@
+import math
+
+
+a=float(input('num1'))
+c=str(input('amalgar'))
+b=float(input('num2'))
+if c=='*':
+    print(a*b)
+if c=='+':
+    print(a+b)
+if c=='-':
+    print(a-b)
+if c=='/':
+    print(a/b)
+if c=="**":
+    print(a**b)
+
+
+
+
+
